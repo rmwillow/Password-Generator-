c12263c20b2748f095ba852272e3d0caa382c7fd
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-05-17 14:09:33 -0500

    Dev (#6)
    * added prompts
    
    * generate func, and compare letter sets
    
    * return data with validation
    
    * added README and button functionality
    
    * suggestions added and changed code

--- script.js
+++ script.js
@@ -1,67 +1,35 @@
-var lengthThangs = function() {
-    var promptLength = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
-    // validate prompt Length answer
-    if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
-        window.alert("You need to provide a valid answer! Please try again.");
-        // use return to call it again and stop the rest of this function from running
-        return lengthThangs();
-    }
-    return promptLength;
-}
+//prompts how many character should they want and validates number entered in stores it in var length
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("How many characters would you like your password to be? Length must be between 8 & 128 characters"));
 
-var letterThangs = function() {
-    //evaluate letter type
+//prompts what type of characters they would like 
+var upperCase = confirm("Would you like to use uppercase letters?");
+var lowerCase = confirm("Would you like to use lowercase letters?");
+var numeric = confirm("Would you like to use numeric?");
+var special = confirm("Would you like to use special characters?");
 
-    var letterType = prompt("Enter a which character types: special, numeric, uppercase, lowercase, All");
-    letterType.toLowerCase();
-    if (letterType === "lowercase") {
-        return "abcdefghijklmnopqrstuvwxyz";
-    } else if (letterType === "uppercase") {
-        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else if (letterType === "numeric") {
-        return "0123456789";
-    } else if (letterType === "special") {
-        return " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    } else if (letterType === "all") {
-        return " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    }
-    // validate letter type prompt answer
-    else if (letterType === "" || letterType === null) {
-        window.alert("You need to provide a valid answer! Please try again.");
-        // use return to call it again and stop the rest of this function from running
-        return letterThangs();
 
-    }
-}
-
-//generate password 
-function generatePassword() {
-    //evaluate character type
-    letterSet = letterThangs();
-    // compare to each letter sets 
-    lengthNum = lengthThangs();
-
-    //return value
-    var returnVal = "";
-    for (var i = 0; i < lengthNum; i++) {
-        //picks a character within letterSet at index of random number
-        returnVal += letterSet.charAt(Math.floor(Math.random() * letterSet.length));
-    }
-    return returnVal;
+//if not any below it will not use the character
+while (!upperCase && !lowerCase && !numeric && !special) {
+    alert("You must select at least one character type!");
+    upperCase = confirm("Would you like to use uppercase letters?");
+    lowerCase = confirm("Would you like to use lowercase letters?");
+    numeric = confirm("Would you like to use numeric?");
+    special = confirm("Would you like to use special characters?");
 
 };
+//generates password using rando link below
+//https://randojs.com/
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+    var password = "";
+    var allowed = {};
+    if (upperCase) password += rando(allowed.upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    if (lowerCase) password += rando(allowed.lowerCase = "abcdefghijklmnopqrstuvwxyz");
+    if (numeric) password += rando(allowed.numeric = "0123456789");
+    if (special) password += rando(allowed.special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
 
-// Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+    for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
 
+    document.getElementById("password").value = randoSequence(password).join("");
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
