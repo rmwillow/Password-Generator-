d12372f2689fa57b44d3d412e7c588ae085a4462
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-05-17 09:51:20 -0500

    Dev (#5)
    * added prompts
    
    * generate func, and compare letter sets
    
    * return data with validation
    
    * added README and button functionality

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# friendly-parakeet
+# Password Generator
+
+> **Password Generator** is a website that generates a secure password. With choices and validation.
+<br />
+<br />
+
+![Homepage](https://user-images.githubusercontent.com/52016382/118507311-bb437300-b6f3-11eb-9fc9-5305775a6522.png)
+
+>  Visit [Password Generator](https://rmwillow.github.io/Password-Generator-/)
+ _______________________________________________________________________
+ <br />
+ <br />
+
+## 5 Options of secure passwords.
+
+> - Special Characters
+
+> - Numeric
+
+> - Upper Case
+
+> - Lower Case
+
+> - All
+
+![Options](https://user-images.githubusercontent.com/52016382/118507406-d1e9ca00-b6f3-11eb-91d2-81229a6238d0.png)
+_________________________________________________________________________
+<br />
+<br />
+
+## Choose how long your password will be
+<br />
+<br />
+
+![Length Of Password](https://user-images.githubusercontent.com/52016382/118507510-e9c14e00-b6f3-11eb-9814-49a00db3b852.png)
+
+<br />
+<br />
+
+## See your password generated
+<br />
+<br />
+
+![Generated Password](https://user-images.githubusercontent.com/52016382/118507700-17a69280-b6f4-11eb-83ed-a190056a733d.png)
+
--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-// Assignment code here
+var lengthThangs = function() {
+    var promptLength = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+    // validate prompt Length answer
+    if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        // use return to call it again and stop the rest of this function from running
+        return lengthThangs();
+    }
+    return promptLength;
+}
 
+var letterThangs = function() {
+    //evaluate letter type
+
+    var letterType = prompt("Enter a which character types: special, numeric, uppercase, lowercase, All");
+    letterType.toLowerCase();
+    if (letterType === "lowercase") {
+        return "abcdefghijklmnopqrstuvwxyz";
+    } else if (letterType === "uppercase") {
+        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else if (letterType === "numeric") {
+        return "0123456789";
+    } else if (letterType === "special") {
+        return " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    } else if (letterType === "all") {
+        return " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    }
+    // validate letter type prompt answer
+    else if (letterType === "" || letterType === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        // use return to call it again and stop the rest of this function from running
+        return letterThangs();
+
+    }
+}
+
+//generate password 
+function generatePassword() {
+    //evaluate character type
+    letterSet = letterThangs();
+    // compare to each letter sets 
+    lengthNum = lengthThangs();
+
+    //return value
+    var returnVal = "";
+    for (var i = 0; i < lengthNum; i++) {
+        //picks a character within letterSet at index of random number
+        returnVal += letterSet.charAt(Math.floor(Math.random() * letterSet.length));
+    }
+    return returnVal;
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
